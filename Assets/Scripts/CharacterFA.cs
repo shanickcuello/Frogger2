cd25236bd2e9a2122b1cc30b6acc7abc49fa45c4
shanickcuello <44624042+shanickcuello@users.noreply.github.com>
2021-07-09 00:15:13 -0300

    MetioritoMatoDinosaurio

--- Assets/Scripts/CharacterFA.cs
+++ Assets/Scripts/CharacterFA.cs
@@ -12,36 +12,47 @@ public class CharacterFA : MonoBehaviourPun
     public float speedMovement;
     public Vector3 spawnPos;
     WinTrigger winManager;
+    public GameObject loosePanel;
+    public bool gameStart;
+
 
     private void Awake()
     {
+        gameStart = false;
         GetComponent<SpriteRenderer>().color = Color.red;
     }
 
     public void Move(EJumpDir dir)
     {
-        switch (dir)
+        if (gameStart)
         {
-            case EJumpDir.UP:
-                transform.Translate(Vector3.up * Time.deltaTime * speedMovement);
-                break;
-            case EJumpDir.DOWN:
-                transform.Translate(Vector3.down * Time.deltaTime * speedMovement);
-                break;
-            case EJumpDir.RIGHT:
-                transform.Translate(Vector3.right * Time.deltaTime * speedMovement);
-                break;
-            case EJumpDir.LEFT:
-                transform.Translate(Vector3.left * Time.deltaTime * speedMovement);
-                break;
-            default:
-                break;
+            switch (dir)
+            {
+                case EJumpDir.UP:
+                    transform.Translate(Vector3.up * Time.deltaTime * speedMovement);
+                    break;
+                case EJumpDir.DOWN:
+                    transform.Translate(Vector3.down * Time.deltaTime * speedMovement);
+                    break;
+                case EJumpDir.RIGHT:
+                    transform.Translate(Vector3.right * Time.deltaTime * speedMovement);
+                    break;
+                case EJumpDir.LEFT:
+                    transform.Translate(Vector3.left * Time.deltaTime * speedMovement);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
     private void Update()
     {
         Debug.LogWarning("Mi player es: " + playerId);
+        if (PhotonNetwork.PlayerList.Length > 4)
+        {
+            gameStart = true;
+        }
     }
 
     public CharacterFA SetInitialParameters(Player localPlayer)
@@ -72,6 +83,7 @@ public class CharacterFA : MonoBehaviourPun
 
         if (collision.gameObject.GetComponent<WinTrigger>())
         {
+            Win();
             MyServer.Instance.RequestIWin(playerId);
             Debug.Log("requestee ganar" + " soy: " + playerId);
         }
@@ -82,13 +94,26 @@ public class CharacterFA : MonoBehaviourPun
         transform.position = backPos;
     }
 
+    internal void Win()
+    {
+        Debug.Log("Entre en el metodo win");
+        Instantiate(loosePanel);
+        PhotonNetwork.Instantiate(loosePanel.name, loosePanel.transform.position, transform.rotation);
+        winManager = FindObjectOfType<WinTrigger>();
+        winManager.IWin();
+        Debug.Log("Yo gane");
+        Time.timeScale = 0;
+    }
+
     internal void Win(Player player)
     {
         Debug.Log("Entre en el metodo win");
+        Instantiate(loosePanel);
+        PhotonNetwork.Instantiate(loosePanel.name, loosePanel.transform.position, transform.rotation);
         winManager = FindObjectOfType<WinTrigger>();
         if (player == playerId)
         {
-            Debug.Log( "if (" + player + " == + " + playerId + ")");
+            Debug.Log("if (" + player + " == + " + playerId + ")");
             winManager.IWin();
             Debug.Log("Yo gane");
         }
